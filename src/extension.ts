2e31815a6bd63475febb023bf8535f1577d48c44
Musfirat <farihamusfirat@gmail.com>
2026-09-15 02:30:00 +0600

    finally colour change!!

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+import * as vscode from 'vscode';//API ney
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "shira-yuki23" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('shira-yuki23.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Pastella started!');
 	});
-
-	
-	const editorListener = vscode.window.onDidChangeActiveTextEditor(editor => {
-	if (!editor) return;
-
+	const editorListener = vscode.window.onDidChangeActiveTextEditor(async editor => {
+	if (!editor) {return;}
+	//lang detct kore
 	const lang = editor.document.languageId;
 	console.log("Language Detected:", lang);	
-
+	if(lang =='javascript')
+	{
+		const config= vscode.workspace.getConfiguration('workbench');
+		try{
+			await config.update(
+				'colorTheme', 
+				'Default Dark+', 
+				vscode.ConfigurationTarget.Global
+			);
+			console.log('Theme changed for JS!');
+		}catch(error){
+			console.error('Couldnt change theme:', error);
+		}
+	}
 	});
-
-	context.subscriptions.push(editorListener);//editor listener bcz everyone should be disposed when extension stops 
-
-
+	//theme detc
+	const config= vscode.workspace.getConfiguration('workbench');
+	const currentTheme= config.get<string>('colorTheme');
+	console.log("Current Theme:", currentTheme);
+	context.subscriptions.push(editorListener);
 	context.subscriptions.push(disposable);
 }
-
-// This method is called when your extension is deactivated
 export function deactivate() {}
